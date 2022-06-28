4334bd74a791aad72263b0bede4325cb07e11a72
igort <igor.torrestj11@gmail.com>
2022-06-28 19:18:10 -0300

    criação app estoque e dos models Estoque e EstoqueItens

---
+++ estoque/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import User
+from core.models import TimeStampedModel
+from produto.models import Produto
+
+
+MOVIMENTO = (
+    ('e', 'entrada'),
+    ('s', 'saída'),
+)
+
+class Estoque(TimeStampedModel):
+    funcionario = models.ForeignKey(User, on_delete=models.CASCADE)
+    nf = models.PositiveIntegerField('Nota fiscal', null=True, blank=True)
+    movimento = models.CharField(max_length=1, choices=MOVIMENTO)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return str(self.pk)
+
+class EstoqueItens(models.Model):
+    estoque = models.ForeignKey(Estoque, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    quantidade = models.PositiveIntegerField()
+    saldo - models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ('pk',)
+
+    def __str__(self):
+        return f'{self.pk}-{self.estoque}-{self.produto}'
+
