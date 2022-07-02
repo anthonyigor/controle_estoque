0c2ec703a09ed481c2b462bce6931a49ecf09597
igort <igor.torrestj11@gmail.com>
2022-07-02 19:51:55 -0300

    criação template base e index

---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+import core.views as v
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', v.index, name='index'),
+]
--- create_data.py
+++ create_data.py
@@ -5,7 +5,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto.settings')
 django.setup()
 
 import string
-import timeit
 from random import choice, random, randint
 from produto.models import Produto
 
