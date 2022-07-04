5f73c12a7c3f46e4fd025bd22ea92aa6e3df8a57
igort <igor.torrestj11@gmail.com>
2022-07-04 19:48:25 -0300

    criação do template product_detail

--- produto/urls.py
+++ produto/urls.py
@@ -6,4 +6,5 @@ app_name = 'produto'
 
 urlpatterns = [
     path('', v.product_list.as_view(), name='product_list'),
+    path('<int:pk>/', v.product_detail, name='product_detail'),
 ]
--- produto/views.py
+++ produto/views.py
@@ -1,5 +1,6 @@
 from .models import Produto
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 
 class product_list(TemplateView):
@@ -9,3 +10,10 @@ class product_list(TemplateView):
         context = super().get_context_data(**kwargs)
         context['object_list'] = Produto.objects.all()
         return context
+
+
+def product_detail(request, pk):
+    template_name = 'product_detail.html'
+    obj = Produto.objects.get(pk=pk)
+    context = {'object': obj}
+    return render(request, template_name, context)
