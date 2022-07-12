e93ad024558dc798fd1b4f5c3fe0c16a3e6b393e
Anthony Igor <igor.torrestj11@gmail.com>
2022-07-12 18:58:04 -0300

    add estoque_saida_list.html

--- estoque/views.py
+++ estoque/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, resolve_url
 from django.views.generic import TemplateView
-from .models import Estoque, EstoqueItens, Produto
+from .models import EstoqueEntrada, EstoqueSaida, EstoqueItens, Produto
 from .forms import EstoqueForm, EstoqueItensForm
 from django.forms import inlineformset_factory
 from django.http import HttpResponseRedirect
@@ -11,13 +11,14 @@ class EstoqueEntradaList(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['object_list'] = Estoque.objects.filter(movimento='e')
+        context['object_list'] = EstoqueEntrada.objects.all()
         return context
+
 
 
 def estoque_entrada_detail(request, pk):
     template_name = 'estoque_entrada_detail.html'
-    obj = Estoque.objects.get(pk=pk)
+    obj = EstoqueEntrada.objects.get(pk=pk)
     context = {'object': obj}
     return render(request, template_name, context)
 
@@ -36,12 +37,12 @@ def baixa_estoque(form):
 
 def estoque_entrada_add(request):
     template_name = 'estoque_entrada_form.html'
-    estoque_form = Estoque()
+    estoque_form = EstoqueEntrada()
 
     '''cria a fábrica de forms inline
     param: model, inline_model, form em que serão salvos os dados '''
     item_estoque_formset = inlineformset_factory(
-        Estoque,
+        EstoqueEntrada,
         EstoqueItens,
         form=EstoqueItensForm,
         extra=0,
@@ -64,3 +65,12 @@ def estoque_entrada_add(request):
 
     context = {'form': form, 'formset': formset}
     return render(request, template_name, context)
+
+
+class EstoqueSaidaList(TemplateView):
+    template_name = 'estoque_saida_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['object_list'] = EstoqueSaida.objects.all()
+        return context
