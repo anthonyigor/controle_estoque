6ca68e60c48462bbd8469d5ed72fdc54760ff116
Anthony Igor <igor.torrestj11@gmail.com>
2022-07-20 18:04:40 -0300

    add logout button

--- projeto/urls.py
+++ projeto/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('produto/', include('produto.urls')),
-    path('estoque/', include('estoque.urls'))
+    path('estoque/', include('estoque.urls')),
+    path('logout', LogoutView.as_view(), name='logout')
 ]
